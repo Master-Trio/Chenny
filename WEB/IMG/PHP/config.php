bdd9f4a00ac182467e7ea78e8cc3a6803b104944
Maciej Dzialoszynski <mdzialoszynski@student.tgm.ac.at>
2018-04-06 13:08:07 +0200

    Revert "Revert "Ordnerstruktur""
    This reverts commit 979a877a0b8fbb641b42035301416cc026bf1990.

--- WEB/IMG/PHP/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$servername = "localhost";
-$username = "proj_chenny";
-$password = "Aeth3fai";
-$db="proj_chenny";
-$table="user";	
-$conn = mysqli_connect($servername, $username, $password,$db);
-?>
